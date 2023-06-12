f7678ec6ee380a7399f81755716e26b5a2ac2602
Yuriy Yurinskiy <yuriyyurinskiy@yandex.ru>
2023-06-12 13:06:21 +0700

    feat: добавляет CORS

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
--- src/Controller/Api/MainController.php
+++ src/Controller/Api/MainController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\ContentContract;
+use App\Entity\Contract;
 use App\Entity\User;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +16,23 @@ class MainController extends AbstractController
     #[Route('/api/', name: 'api_main_index')]
     public function index(#[CurrentUser] User $user): Response
     {
+        $permissions = [];
+
+        $today = new DateTime('midnight');
+
+        $filterContracts = fn(Contract $contract) => $today >= $contract->getCreatedAt() && ($today <= $contract->getRemovedAt() || null === $contract->getRemovedAt());
+        $filterContents = fn(ContentContract $content) => $today >= $content->getCreatedAt() && ($today <= $content->getRemovedAt() || null === $content->getRemovedAt());
+
+        foreach ($user->getContracts()->filter($filterContracts) as $contract) {
+            foreach ($contract->getContents()->filter($filterContents) as $content) {
+                $permissions[] = $content->getLabWork()->getSystemName();
+            }
+        }
+
         return $this->json([
-            'username' => $user->getUsername()
+            'username' => $user->getUsername(),
+            'fio' => $user->getEmail(),
+            'permissions' => $permissions
         ]);
     }
 }
